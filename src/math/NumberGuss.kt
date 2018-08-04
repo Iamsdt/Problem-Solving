4dd91b15b49243b0bc31fcb640661efbab7233d2
Iamsdt <shudiptotrafder@gmail.com>
2018-08-04 22:14:54 +0600

    Number Guss
    Took 1 minute

--- src/math/NumberGuss.kt
+++ src/math/NumberGuss.kt
@@ -1,5 +1,27 @@
 package math
 
+/*
+Find the logic from the given sample input/output.
+
+And answer Q queries.
+
+Constraints :
+
+1 <= Value <= 100000
+1<=nunber of query<=10000
+
+SAMPLE INPUT   SAMPLE OUTPUT
+8
+10  8
+30  42
+45  33
+9   4
+69  27
+77  19
+127 1
+150 222
+ */
+
 fun main(args: Array<String>) {
 
     val times = readLine()?.toInt() ?: 0
