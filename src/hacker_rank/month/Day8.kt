b85d599829e4a38d036c43ed4db73f05c1281bd3
Shudipto <Shudiptotrafder@gmail.com>
2018-09-05 23:58:15 +0600

    try catch
    Took 10 minutes

--- src/hacker_rank/month/Day8.kt
+++ src/hacker_rank/month/Day8.kt
@@ -7,19 +7,23 @@ fun main(args: Array<String>) {
 
     val map:HashMap<String,Int> = hashMapOf()
 
-    for (n in 0 until time) {
-        val add = readLine() ?: ""
-        val li = add.split(" ")
-        map.put(li[0],li[1].toInt())
-    }
-
-    for (n in 0 until time) {
-        val key = readLine() ?: ""
-        if (map.containsKey(key)){
-            println("$key=${map[key]}")
-        } else{
-            println("Not found")
+    try{
+        for (n in 0 until time) {
+            val add = readLine() ?: ""
+            val li = add.split(" ")
+            map[li[0]] = li[1].toInt()
         }
+
+        for (n in 0 until time) {
+            val key = readLine() ?: ""
+            if (map.containsKey(key)){
+                println("$key=${map[key]}")
+            } else{
+                println("Not found")
+            }
+        }
+    } catch (e:Exception){
+
     }
 
 }
